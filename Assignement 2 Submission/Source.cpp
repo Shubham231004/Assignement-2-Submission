d92369f9761b94615418a5b9f8e3bdd3cdeccddf
Shubham Patel <spatel6205@conestogac.on.ca>
2024-09-19 22:59:09 -0400

    Final structuring of the code and added comments to the code. Made the code more readable, clean and consistent.

--- Assignement 2 Submission/Source.cpp
+++ Assignement 2 Submission/Source.cpp
@@ -1,67 +1,83 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
+#include <vector>     // To use the std::vector container
 #include <string>
 
+// Struct to store student data
 struct STUDENT_DATA {
-    std::string lname; //declaring the last name variable
-    std::string fname; // declaring the first name variable
-    std::string email; // For pre-release mode
+    std::string lname;  // Declaring the last name variable
+    std::string fname;  // Declaring the first name variable
+    std::string email;  // Email variable (only used in pre-release mode)
 };
 
 int main() {
-    std::vector<STUDENT_DATA> students;
+    std::vector<STUDENT_DATA> students;  // Vector to store student data
 
+    // Check for pre-release mode and open the corresponding file
 #ifdef PRE_RELEASE
-    std::cout << "Running in Pre-Release mode..." << std::endl;
-    std::ifstream file("D:\\Shubham 1\\Conestoga\\Semester 5\\Advanced Software Development\\Assignement 2 Submission\\StudentData_Emails.txt");
+    std::cout << "Running in Pre-Release mode..." << std::endl;  // Notify user of pre-release mode
+    std::ifstream file("D:\\Shubham 1\\Conestoga\\Semester 5\\Advanced Software Development\\Assignement 2 Submission\\StudentData_Emails.txt");  // Open file with email data
 #else
-    std::cout << "Running in Standard mode..." << std::endl;
-    std::ifstream file("D:\\Shubham 1\\Conestoga\\Semester 5\\Advanced Software Development\\Assignement 2 Submission\\StudentData.txt");
+    std::cout << "Running in Standard mode..." << std::endl;  // Notify user of standard mode
+    std::ifstream file("D:\\Shubham 1\\Conestoga\\Semester 5\\Advanced Software Development\\Assignement 2 Submission\\StudentData.txt");  // Open file without email data
 #endif
 
-    std::string line;
+    std::string line;  // Variable to hold each line of the file
 
+    // Check if file was successfully opened
     if (file.is_open()) {
+        // Loop through each line in the file
         while (getline(file, line)) {
-            std::istringstream ss(line);
-            std::string lastName, firstName, email;
+            std::istringstream ss(line);  // Create a string stream to parse the line
+            std::string lastName, firstName, email;  // Variables to store parsed data
 
+            // Extract last name and first name, delimited by a comma
             getline(ss, lastName, ',');
             getline(ss, firstName, ',');
 
 #ifdef PRE_RELEASE
-            getline(ss, email); // Read email only in pre-release mode
+            getline(ss, email);  // Read email if in pre-release mode
 #endif
 
-            STUDENT_DATA student;
-            student.lname = lastName;
-            student.fname = firstName;
+            STUDENT_DATA student;  // Create a new STUDENT_DATA object
+            student.lname = lastName;  // Assign last name to the object
+            student.fname = firstName;  // Assign first name to the object
 
 #ifdef PRE_RELEASE
-            student.email = email;
+            student.email = email;  // Assign email to the object in pre-release mode
 #endif
 
+            // Add the student object to the vector
             students.push_back(student);
         }
-        file.close();
+        file.close();  // Close the file after reading
     }
     else {
-        std::cerr << "Unable to open file" << std::endl;
+        std::cerr << "Oops! Error opening the file" << std::endl;  // Print error message if file can't be opened
     }
 
-    // Print the student data
-#ifdef _DEBUG
-    std::cout << "Debug Mode: Printing student information..." << std::endl;
+    // Check if the program is not in debug mode and is in pre-release mode
+#if !defined(_DEBUG) && defined(PRE_RELEASE)
+    // Loop through the vector of students and print only the email addresses
     for (const auto& student : students) {
-        std::cout << student.fname << " " << student.lname;
-#ifdef PRE_RELEASE
-        std::cout << " - " << student.email; // Only print email in pre-release mode
-#endif
-        std::cout << std::endl;
+        std::cout << student.email << std::endl;  // Output the student's email
     }
 #endif
 
-    return 0;
+
+    // If in debug mode, print the student data for verification
+#ifdef _DEBUG
+    for (const auto& student : students) {
+        // Print first name and last name
+        std::cout << student.fname << " " << student.lname;
+
+#ifdef PRE_RELEASE
+        std::cout << " - " << student.email;  // Print email only in pre-release mode
+#endif
+        std::cout << std::endl;  // Print a new line after each student's data
+    }
+#endif
+
+    return 0;  // Exit the program successfully
 }
